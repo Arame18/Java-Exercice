ee30529a66a0e515115eb59636c5ac6667097df4
Arame18 <78623719+Arame18@users.noreply.github.com>
2021-03-26 11:17:07 +0000

    Add files via upload

---
+++ ExerciceMameArameKebeGlrs2b.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+class Anneebisextile{
+    public static void main(String[] args)throws Exception{
+        Scanner sc=new Scanner(System.in);//saisir
+        /*
+        Exercice 1:
+        Saisir une année qui est un entier positif puis 
+        determiner si l'année est bisextile ou pas 
+        une année est bissextile si elle est :
+            -divisible par 4 et non par 100
+            -ou divisible par 400
+        */
+System.out.print("entrer anne :\t");
+
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+           Exercice 2 :{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	{
+			SimpleDateFormat formater = null;
+			Date aujourdhui = new Date();
+			formater = new SimpleDateFormat("dd-MM-yy");
+		    System.out.println(formater.format(aujourdhui));
+
+		
+	
+	}
+}}
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+         Exercice 3: {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	{
+			SimpleDateFormat formater = null;
+			Date aujourdhui = new Date();
+			formater = new SimpleDateFormat("dd-MM-yyyy");
+		    System.out.println(formater.format(aujourdhui));
+
+		
+	
+	}
+}}
+
